47c017c2416a7620c53ab0bc1aa165f639f743ef
danischm <danischm@cisco.com>
2021-10-11 20:33:51 +0200

    Update implicit resource dependencies

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aci_rest" "aaaUser" {
 
 resource "aci_rest" "aaaUserDomain" {
   for_each   = { for domain in var.domains : domain.name => domain }
-  dn         = "${aci_rest.aaaUser.id}/userdomain-${each.value.name}"
+  dn         = "${aci_rest.aaaUser.dn}/userdomain-${each.value.name}"
   class_name = "aaaUserDomain"
   content = {
     name = each.value.name
@@ -58,7 +58,7 @@ resource "aci_rest" "aaaUserDomain" {
 
 resource "aci_rest" "aaaUserRole" {
   for_each   = { for role in local.roles : role.key => role.value }
-  dn         = "${aci_rest.aaaUserDomain[each.value.domain_name].id}/role-${each.value.name}"
+  dn         = "${aci_rest.aaaUserDomain[each.value.domain_name].dn}/role-${each.value.name}"
   class_name = "aaaUserRole"
   content = {
     name     = each.value.name
@@ -68,7 +68,7 @@ resource "aci_rest" "aaaUserRole" {
 
 resource "aci_rest" "aaaUserCert" {
   for_each   = { for cert in var.certificates : cert.name => cert }
-  dn         = "${aci_rest.aaaUser.id}/usercert-${each.value.name}"
+  dn         = "${aci_rest.aaaUser.dn}/usercert-${each.value.name}"
   class_name = "aaaUserCert"
   content = {
     name = each.value.name
@@ -78,7 +78,7 @@ resource "aci_rest" "aaaUserCert" {
 
 resource "aci_rest" "aaaSshAuth" {
   for_each   = { for key in var.ssh_keys : key.name => key }
-  dn         = "${aci_rest.aaaUser.id}/sshauth-${each.value.name}"
+  dn         = "${aci_rest.aaaUser.dn}/sshauth-${each.value.name}"
   class_name = "aaaSshAuth"
   content = {
     name = each.value.name
